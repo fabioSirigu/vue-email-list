906b06b86e605bc98a82ab2962789465599e3332
naz1ne <fabio.sirigu.1995@gmail.com>
2022-11-08 13:55:18 +0100

    initial and final commit

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+/* Descrizione:
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+
+const { createApp } = Vue
+
+createApp ({
+      data(){
+            return {
+                  urlMail: 'https://flynn.boolean.careers/exercises/api/random/mail',
+                  mailArray: []
+            }
+      },
+      mounted(){
+            for (let index = 0; index < 10; index++) {
+                  axios
+                  .get(this.urlMail)
+                  .then(response => {
+                        this.mailArray.push(response.data.response)
+                  })     
+            }
+      }
+}).mount('#app')
